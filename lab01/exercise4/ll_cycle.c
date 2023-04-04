a7e0ebc471e16606b0f10a8612a6f2005ec85132
Ammar Khan <ammar.sarwar@10xengineers.ai>
2023-04-04 12:50:47 +0500

    Exercise 4 solution has been added

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,24 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+     /* TODO: Implement ll_has_cycle */
+    
+    if (head == NULL) { //check if the list is empty or not
+        return 0;
+    }
+    
+    node *slow_ptr = head;
+    node *fast_ptr = head->next;
+    
+    while (head != NULL && fast_ptr != NULL && fast_ptr->next != NULL) {
+        if (fast_ptr == slow_ptr || fast_ptr->next == slow_ptr) {
+            return 1; 
+        }
+        if (slow_ptr->next !=NULL){
+          slow_ptr = slow_ptr->next;
+        }
+          fast_ptr = fast_ptr->next->next;
+    }
+
+    return 0;
 }
